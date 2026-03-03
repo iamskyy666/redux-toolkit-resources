4572d8a385da21ac75f59e7868ebddeb015a3654
Soumadip <39thdecember@gmail.com>
2026-03-04 03:00:18 +0530

    Redux Devtools + loggerMiddleWare

--- src/store/store.js
+++ src/store/store.js
@@ -2,11 +2,21 @@ import { configureStore } from "@reduxjs/toolkit";
 import counterReducer from "../features/counter/counterSlice";
 import authReducer from "../features/auth/authSlice";
 
+// logger MW if we don't wanna use the React/Redux Devtools
+const loggerMiddleware = (storeAPI) => (next) => (action) => {
+  console.log(`Dispatching action:`, action);
+  const result = next(action);
+  console.log(`Next state:`, storeAPI.getState());
+  return result;
+};
+
 export const store = configureStore({
   reducer: {
     // all reducers here, based on slice(s)/features
     counter: counterReducer,
     auth: authReducer,
   },
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware().concat(loggerMiddleware),
+  // devTools:false
 });
-
